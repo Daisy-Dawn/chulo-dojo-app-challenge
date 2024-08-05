fd9ca935d473cd6a91cc0d0c3e5d4ebbe8f6c4ca
Daisy-Dawn <agboella20@gmail.com>
2024-08-06 00:17:12 +0100

    updates

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -11,14 +11,24 @@ import { getDatabase } from 'firebase/database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyBrbrLV3RpkxUjnRGP3AT-8ExxOpSoQnb4',
-    authDomain: 'chulo-dojo-app.firebaseapp.com',
-    databaseURL: 'https://chulo-dojo-app-default-rtdb.firebaseio.com',
-    projectId: 'chulo-dojo-app',
-    storageBucket: 'chulo-dojo-app.appspot.com',
-    messagingSenderId: '856274412831',
-    appId: '1:856274412831:web:4df6b5e0bc3d93c129fc2a',
+    apiKey: 'AIzaSyCuW3f3H9PGZtHSRLqP7RmCV-8D4z1Hjx8',
+    authDomain: 'daisy-77b9d.firebaseapp.com',
+    projectId: 'daisy-77b9d',
+    storageBucket: 'daisy-77b9d.appspot.com',
+    messagingSenderId: '280940112842',
+    appId: '1:280940112842:web:c71be3fee06322881bfd33',
+    measurementId: 'G-RY7T7VK9FZ',
 }
+
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyBrbrLV3RpkxUjnRGP3AT-8ExxOpSoQnb4',
+//     authDomain: 'chulo-dojo-app.firebaseapp.com',
+//     databaseURL: 'https://chulo-dojo-app-default-rtdb.firebaseio.com',
+//     projectId: 'chulo-dojo-app',
+//     storageBucket: 'chulo-dojo-app.appspot.com',
+//     messagingSenderId: '856274412831',
+//     appId: '1:856274412831:web:4df6b5e0bc3d93c129fc2a',
+// }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
